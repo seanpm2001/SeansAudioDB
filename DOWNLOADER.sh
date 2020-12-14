e6073c86145627abdc028cabed01dc179f2e8fee
Sean P. Myrick V19.1.7.2 <65933340+seanpm2001@users.noreply.github.com>
2020-12-13 20:18:38 -0800

    DOWNLOADER.sh
    The video downloader I use and how to install it.

---
+++ DOWNLOADER.sh
@@ -0,0 +1,19 @@
+# Download the video downloader I use
+# Ubuntu method (old)
+def installUbuntuOldMethod() {
+sudo apt-get youtube-dl
+}
+# Ubuntu method (new)
+def installUbuntuNewMethod() {
+sudo apt install youtube-dl
+}
+# Fedora method (old)
+def installFedoraNewMethod() {
+sudo dnf install youtube-dl
+}
+# Fedora method (new)
+def installFedoraNewMethod() {
+sudo yum install youtube-dl
+}
+# Other video player (alternate for rare cases, or when YouTube-DL goes down to a false DMCA claim)
+# Firefox: VideoDownloadHelper
