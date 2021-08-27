e324189c1d4701047c12fc02f27bc47af395e66e
Sean P. Myrick V19.1.7.2 <65933340+seanpm2001@users.noreply.github.com>
2021-08-26 19:17:01 -0700

    makefile.mk (V2)
    Version 2 of the makefile for this project.

--- makefile.mk
+++ makefile.mk
@@ -5,9 +5,26 @@
 copy /COPYINGL to /COPYING
 rm -f /COPYINGL
 echo "COPYING license file has been corrected."
+wait 50
+break
+# For fixing the music directory issue
+# echo "WARNING! This action involves copying and deleting over 300 gigabytes of data, and over 20000 files. This may take time and resources. Do you wish to continue?")
+#wait
+#copy ...
+# Wait, I have a better idea!
+copy /Audio to /audio
+wait 1000 # Waits 1000 milliseconds (1 second) before continuing
+rename /Music to /"Audio"
+wait 1000 # See above
+rename /audio to /Audio
+wait 50
+break
+# I can't find a more efficient way
+# This is pseudocode and likely will not work.
+
 # This syntax isn't valid yet, I don't know how to write in GNU Make very well
 # File info
 # File type: Makefile (MAKEFILE, *.mk)
-# File version: 1 (Wednesday, August 18th 2021 at 6:41 pm)
-# Line count (including blank lines and compiler line): 14
+# File version: 2 (Thursday, August 26th 2021 at 7:17 pm)
+# Line count (including blank lines and compiler line): 31
 # End of script
