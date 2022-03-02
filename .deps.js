5cf6f1168fafe415a10db2969913fef0d3c2c558
Sean P. Myrick V19.1.7.2 <seanwallawalla@protonmail.com>
2022-03-01 17:56:02 -0800

    .deps.js [Dependencies] [V2]
    An archive of the 2nf version of the dependencies file for this project.

--- .deps.js
+++ .deps.js
@@ -1,4 +1,5 @@
 // Dependencies for seanpm2001/SeansAudioDB
+
 // HTML version: 5.0 or later
 // JQuery version: 1.12
 // CSS version: 3.0 or later
@@ -24,9 +25,41 @@
 // Emoji version: 10.0 or later
 // CFF version: 1.2.0
 // BASH shell version: 1.0 or later
+// ReStructured Text version: 1.0 or later
+// MediaWiki text version: 5.0 or later
 // WebP version (optional) 1.0 or later
+
+console.log("HTML version: 5.0 or later");
+console.log("JQuery version: 1.12");
+console.log("CSS version: 3.0 or later");
+console.log("JavaScript version: 1.8 or later");
+console.log("Markdown (CommonMark)");
+console.log("GNU Make version: 1.0 or later");
+console.log("JSON version: Not set");
+console.log("Android (optional): NOMEDIA support");
+console.log("INI version: Not set");
+console.log(".Desktop file version: Not set");
+console.log("LaTeX version: 2.0 or later");
+console.log("SubRip Text Version: Not set");
+console.log("YAML version: 1.1");
+console.log("SVG version: 1.1 or later");
+console.log("PNG version: Any");
+console.log("JPEG Version: Any");
+console.log("VLC Media Player version: 1.0 or later");
+console.log("GIF version: 1.1");
+console.log("Web browser: Firefox or other WHATWG compatible web browser");
+console.log("XML version: 1.1");
+console.log("BitMap version: Any");
+console.log("Git version: 1.0 or later");
+console.log("Emoji version: 10.0 or later");
+console.log("CFF version: 1.2.0");
+console.log("BASH shell version: 1.0 or later");
+console.log("ReStructured Text version: 1.0 or later");
+console.log("MediaWiki text version: 5.0 or later");
+console.log("WebP version (optional) 1.0 or later");
+break;
 
 // File info
 // File type: JavaScript source file (*.js)
-// File version: 1 (2022, Tuesday, March 1st at 5:39 pm)
-// Line count (including blank lines and compiler line): 33
+// File version: 2 (2022, Tuesday, March 1st at 5:56 pm)
+// Line count (including blank lines and compiler line): 66
