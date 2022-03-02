ae6d602e77aa7c9258a16deeab7fcc07de961f04
Sean P. Myrick V19.1.7.2 <seanwallawalla@protonmail.com>
2022-03-01 17:39:01 -0800

    .deps.js [Dependencies] [V1]
    Version 1 of the Dependencies file for this project.

---
+++ .deps.js
@@ -0,0 +1,32 @@
+// Dependencies for seanpm2001/SeansAudioDB
+// HTML version: 5.0 or later
+// JQuery version: 1.12
+// CSS version: 3.0 or later
+// JavaScript version: 1.8 or later
+// Markdown (CommonMark)
+// GNU Make version: 1.0 or later
+// JSON version: Not set
+// Android (optional): NOMEDIA support
+// INI version: Not set
+// .Desktop file version: Not set
+// LaTeX version: 2.0 or later
+// SubRip Text Version: Not set
+// YAML version: 1.1
+// SVG version: 1.1 or later
+// PNG version: Any
+// JPEG Version: Any
+// VLC Media Player version: 1.0 or later
+// GIF version: 1.1
+// Web browser: Firefox or other WHATWG compatible web browser
+// XML version: 1.1
+// BitMap version: Any
+// Git version: 1.0 or later
+// Emoji version: 10.0 or later
+// CFF version: 1.2.0
+// BASH shell version: 1.0 or later
+// WebP version (optional) 1.0 or later
+
+// File info
+// File type: JavaScript source file (*.js)
+// File version: 1 (2022, Tuesday, March 1st at 5:39 pm)
+// Line count (including blank lines and compiler line): 33
