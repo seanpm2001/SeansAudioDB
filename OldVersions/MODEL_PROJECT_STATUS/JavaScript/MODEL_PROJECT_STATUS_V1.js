00a4caeb0a3fa1a06e230985bf38b26ce4b5f181
Sean P. Myrick V19.1.7.2 <seanwallawalla@protonmail.com>
2022-03-25 17:55:52 -0700

    MODEL_PROJECT_STATUS_V1.js
    An archive of the first version of the JavaScript version of the model project status file for this project.

---
+++ OldVersions/MODEL_PROJECT_STATUS/JavaScript/MODEL_PROJECT_STATUS_V1.js
@@ -0,0 +1,29 @@
+ // Start of script
+alert("The HTML version of this document is preferred, but since you already running it as JavaScript, please open your web console by pressing F12 or CTRL + F12");
+console.log("Model project status for SeansAudioDB");
+console.log("This is one of @seanpm2001's Model projects. It is certified as a project ready for use as a first impression into seanpm2001 and his works");
+console.log("What does this mean?");
+console.log("This means that this project is highly developed, and is constantly improved upon. It is verified to be used as a starter project when introducing someone to Seans works. Out of the 1500+ projects Sean has, less than 20 have model project status.");
+console.log("Other model projects");
+console.log("Here is a list of other model projects by @seanpm2001 as of 2022 Saturday March 12th:");
+console.log("https://github.com/seanpm2001/seanpm2001/");
+console.log("https://github.com/seanpm2001/GitHub_Organization_Info/");
+console.log("https://github.com/seanpm2001/SeansAudioDB/ (current)");
+console.log("https://github.com/seanpm2001/SNU/");
+console.log("https://github.com/seanpm2001/SNU_2D_ProgrammingTools/");
+console.log("https://github.com/seanpm2001/Git-Templates/");
+console.log("https://github.com/seanpm2001/Degoogle-your-life/");
+console.log("https://github.com/seanpm2001/WacOS/");
+console.log("https://github.com/seanpm2001/Code-distancing/");
+console.log("https://github.com/seanpm2001/Its-time-to-cut-WideVine-DRM/");
+console.log("https://github.com/seanpm2001/MCPYE/");
+console.log("https://github.com/seanpm2001/SeansLifeArchive/");
+console.log("https://github.com/seanpm2001/My-Linux-setup/");
+break;
+/*
+File info</H3>
+File type: JavaScript source file (*.js)
+File version 1 (2022, Friday, March 25th at 5:55 pm)
+Line count (including blank lines and compiler line): 30
+*/
+// End of script
