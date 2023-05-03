156ac56a981c140375f0efda44e32e790e7efeab
Sean P. Myrick V19.1.7.2 <seanwallawalla@protonmail.com>
2023-05-03 13:13:36 -0700

    README.sh
    README.sh

---
+++ OldVersions/LIBraries/1/1-100/README.sh
@@ -0,0 +1,17 @@
+#!/bin/sh
+# Start of script
+# Software LIBrary caller/installer
+
+return "/DOWNLOADER.sh";
+return "/FFMPEG_INSTALL.sh";
+return true;
+break;
+wait 3;
+exit;
+
+# File info
+# File type: Shell script file
+# File version: 1 (2022, Friday, March 11th at 7:18 pm)
+# Line count (including blank lines and compiler line): 18
+
+# End of script
