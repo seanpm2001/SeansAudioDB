c47183208f523fbe1b8a4bd152b40079c40e99ea
Sean P. Myrick V19.1.7.2 <seanwallawalla@protonmail.com>
2025-08-25 19:23:56 -0700

    1 sub-directory [16 files]

---
+++ Music/1.1 Music (GitHub)/Part 2/20Essentials_GitHub/Unsorted/max-bhron-main/config.js
@@ -0,0 +1,23 @@
+export const nameBand = 'MAX BHRON';
+export const nameSongs = [
+	"CYBERPUNK", /*1*/
+	"AI", /*2*/
+	"WAR MACHINE", /*3*/
+	"REDEMPTION", /*4*/
+	"PAIN", /*5*/
+	"ILLUSION", /*6*/
+	"THE FUTURE", /*7*/
+	"HUMANITY", /*8*/
+	"CYBERBLADE", /*9*/
+	"MIDTEMPO BASS BOOSTED", /*10*/
+	"INTERSTELLAR", /*11*/
+	"CAN'T TAKE THIS", /*12*/
+	"ELON TRUST", /*13*/
+	"PSYCHOTROPIC", /*14*/
+	"DARK SOUL", /*15*/
+	"BARTA", /*16*/
+	"MAKE THIS RIGHT", /*17*/
+	"SIBERIAN", /*18*/
+	"MY ONLY CHANCE", /*19*/
+	"FULL CONFESSION", /*20*/
+]
