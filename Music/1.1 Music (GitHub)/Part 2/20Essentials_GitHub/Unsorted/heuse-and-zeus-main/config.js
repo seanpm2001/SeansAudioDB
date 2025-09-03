49956387848859ce2b5ac261dc0e6fae7600ab43
Sean P. Myrick V19.1.7.2 <seanwallawalla@protonmail.com>
2025-09-02 18:25:31 -0700

    1 sub-directory [16 files]

---
+++ Music/1.1 Music (GitHub)/Part 2/20Essentials_GitHub/Unsorted/heuse-and-zeus-main/config.js
@@ -0,0 +1,23 @@
+export const nameBand = 'HEUSE & ZEUS';
+export const nameSongs = [
+	"PILL", /*1*/
+	"GOING INSANE", /*2*/
+	"I'LL BE HERE", /*3*/
+	"TIME", /*4*/
+	"DON'T HOLD ME DOWN", /*5*/
+	"CIRCLES", /*6*/
+	"DANCING ON ICE", /*7*/
+	"LILAC", /*8*/
+	"SPIRITS SAY", /*9*/
+	"BLACK BOOTS", /*10*/
+	"ROSE GOLD", /*11*/
+	"DRIVE", /*12*/
+	"ECHOES", /*13*/
+	"AERIAL", /*14*/
+	"DREAMING", /*15*/
+	"REVERIE", /*16*/
+	"HATE TO LOVE", /*17*/
+	"ALIVE", /*18*/
+	"SYMPHONY", /*19*/
+	"PHASE WITH ME", /*20*/
+]
