11a4739cf2a633644210d3874991e23361b83d42
Sean P. Myrick V19.1.7.2 <seanwallawalla@protonmail.com>
2025-09-08 18:00:37 -0700

    1 sub-directory [16 files]

---
+++ Music/1.1 Music (GitHub)/Part 2/20Essentials_GitHub/Unsorted/ann4-c4rina-main/config.js
@@ -0,0 +1,23 @@
+export const nameBand = 'ANNA CARINA';
+export const nameSongs = [
+	"ESTOY AMANDOTE", /*1*/
+	"DIME SI ESTO ES AMOR", /*2*/
+	"QUIERO CONTIGO", /*3*/
+	"HIPOCRESÍA", /*4*/
+	"CALLAO", /*5*/
+	"TU AMOR ME DUELE", /*6*/
+	"DÓNDE ESTÁN", /*7*/
+	"ME CANSÉ ", /*8*/
+	"SOLITA", /*9*/
+	"DAME TU CARIÑO", /*10*/
+	"PROHIBIDO", /*11*/
+	"TE QUISE MAS QUE NADIE", /*12*/
+	"TÚ Y YO", /*13*/
+	"CHA CHA CHA", /*14*/
+	"SÓLO TÚ", /*15*/
+	"CIELO SIN LUZ", /*16*/
+	"NOCIVO", /*17*/
+	"VOLVER A AMAR", /*18*/
+	"AMÁNDOTE (SOLA)", /*19*/
+	"DIME", /*20*/
+]
