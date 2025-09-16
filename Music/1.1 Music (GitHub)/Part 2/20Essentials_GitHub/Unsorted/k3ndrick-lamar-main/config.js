dad162d085dc5c627b378cbd3c8c379b3684d2b9
Sean P. Myrick V19.1.7.2 <seanwallawalla@protonmail.com>
2025-09-15 19:25:26 -0700

    2 sub-directories [32 files]

---
+++ Music/1.1 Music (GitHub)/Part 2/20Essentials_GitHub/Unsorted/k3ndrick-lamar-main/config.js
@@ -0,0 +1,23 @@
+export const nameBand = 'KENDRICK LAMAR';
+export const nameSongs = [
+	"ALL THE STARS", /*1*/
+	"HUMBLE", /*2*/
+	"NOT LIKE US", /*3*/
+	"DNA", /*4*/
+	"BITCH, DON'T KILL MY VIBE", /*5*/
+	"SWIMMING POOLS", /*6*/
+	"LOYALTY", /*7*/
+	"LOVE", /*8*/
+	"ALRIGHT", /*9*/
+	"KING KUNTA", /*10*/
+	"LUTHER", /*11*/
+	"ELEMENT", /*12*/
+	"TV OFF", /*13*/
+	"THE RECIPE", /*14*/
+	"BACKSEAT FREESTYLE", /*15*/
+	"PRAY FOR ME", /*16*/
+	"RICH SPIRIT", /*17*/
+	"COUNT ME OUT", /*18*/
+	"UNITED IN GRIEF", /*19*/
+	"SILEN HILL", /*20*/
+]
---
+++ Music/1.1 Music (GitHub)/Part 2/20Essentials_GitHub/Unsorted/sebastian-ingrosso-main/config.js
@@ -0,0 +1,23 @@
+export const nameBand = 'SEBASTIAN INGROSSO';
+export const nameSongs = [
+	"RELOAD", /*1*/
+	"DARK RIVER", /*2*/
+	"A NEW DAY", /*3*/
+	"SOMETHING NEW", /*4*/
+	"RELOAD (LIVE)", /*5*/
+	"FLOOD", /*6*/
+	"RIDE IT", /*7*/
+	"FLAGS", /*8*/
+	"SKIP", /*9*/
+	"FLOOD", /*10*/
+	"BURMEISTER", /*11*/
+	"MIAMI", /*12*/
+	"DJ MAG", /*13*/
+	"DÜSSELDORF", /*14*/
+	"LIVE AT BRIXTON", /*15*/
+	"CALLING", /*16*/
+	"NO ENEMIES", /*17*/
+	"U OK? (EXTENDED MIX)", /*18*/
+	"CHURCH", /*19*/
+	"SPEAK UP", /*20*/
+]
