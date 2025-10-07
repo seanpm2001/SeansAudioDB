9e13a21917f520b1ca45ddecf8be93fadc6fa448
Sean P. Myrick V19.1.7.2 <seanwallawalla@protonmail.com>
2025-10-06 21:02:22 -0700

    1 sub-directory [16 files]

---
+++ Music/1.1 Music (GitHub)/Part 2/20Essentials_GitHub/Unsorted/chris-linton-main/config.js
@@ -0,0 +1,23 @@
+export const nameBand = 'CHRIS LINTON';
+export const nameSongs = [
+	"ASCEND", /*1*/
+	"FEARLESS", /*2*/
+	"FEARLESS PT II", /*3*/
+	"SUPERHERO", /*4*/
+	"HOLDING ON", /*5*/
+	"HOMECOMING", /*6*/
+	"MONSTER", /*7*/
+	"FOCUS", /*8*/
+	"WITHOUT YOU", /*9*/
+	"BEST PART", /*10*/
+	"WATCH THE WORLD BURN", /*11*/
+	"SLOW DOWN", /*12*/
+	"LOST", /*13*/
+	"FALL TO THE FLOOR", /*14*/
+	"WHERE ARE WE NOW", /*15*/
+	"UNBREAKABLE", /*16*/
+	"SOS", /*17*/
+	"CALLING", /*18*/
+	"SPELLBOUND", /*19*/
+	"SINKING", /*20*/
+]
