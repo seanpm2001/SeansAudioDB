e5bc1b9aed6f621634ca8ee5abc88781531ea156
Sean P. Myrick V19.1.7.2 <seanwallawalla@protonmail.com>
2025-10-13 19:18:14 -0700

    1 sub-directory [16 files]

---
+++ Music/1.1 Music (GitHub)/Part 2/20Essentials_GitHub/Unsorted/madcon-main/config.js
@@ -0,0 +1,23 @@
+export const nameBand = 'MADCON';
+export const nameSongs = [
+	"DON'T WORRY", /*1*/
+	"GLOW", /*2*/
+	"BEGGIN", /*3*/
+	"DON'T WORRY (RADIO VERSION)", /*4*/
+	"FREAKY LIKE ME", /*5*/
+	"IN MY HEAD", /*6*/
+	"KJØRER PÅ", /*7*/
+	"ONE LIFE", /*8*/
+	"LIAR", /*9*/
+	"STORI", /*10*/
+	"OUTRUN THE SUN", /*11*/
+	"FÅKKE FLY BORT", /*12*/
+	"BACK ON THE ROAD", /*13*/
+	"ARE YOU READY", /*14*/
+	"SNU DEG RUNDT", /*15*/
+	"DROWNING", /*16*/
+	"HELLUVA NITE", /*17*/
+	"SIRE SOM DET ER", /*18*/
+	"GLOW (MORRY REMIX)", /*19*/
+	"FEST PÅ SMESTAD VEST ", /*20*/
+]
