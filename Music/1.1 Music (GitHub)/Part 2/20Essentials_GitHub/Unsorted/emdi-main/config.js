6306656e3f114f68f9aee1e2419962ef862ca7e7
Sean P. Myrick V19.1.7.2 <seanwallawalla@protonmail.com>
2025-10-20 19:12:30 -0700

    1 sub-directory [16 files]

---
+++ Music/1.1 Music (GitHub)/Part 2/20Essentials_GitHub/Unsorted/emdi-main/config.js
@@ -0,0 +1,23 @@
+export const nameBand = 'EMDI';
+export const nameSongs = [
+	"HURTS LIKE THIS", /*1*/
+	"LET YOUR HEARTBREAK", /*2*/
+	"LONEWOLF", /*3*/
+	"MY LOVE", /*4*/
+	"HOUSE", /*5*/
+	"EVOLUTION", /*6*/
+	"DO YA THING", /*7*/
+	"THANK U NEXT", /*8*/
+	"DEMONS", /*9*/
+	"DEEP", /*10*/
+	"HEARTBEAT", /*11*/
+	"GODDESS", /*12*/
+	"ROSES", /*13*/
+	"TEARS", /*14*/
+	"SOMEBODY ELSE", /*15*/
+	"GIVE IT TO ME", /*16*/
+	"HEART LESS", /*17*/
+	"THE SCIENTIST", /*18*/
+	"ON MY MIND", /*19*/
+	"DRIVE", /*20*/
+]
