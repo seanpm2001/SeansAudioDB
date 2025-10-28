aa23ebbd39584f1029024a9e825ab05c9dd83367
Sean P. Myrick V19.1.7.2 <seanwallawalla@protonmail.com>
2025-10-27 20:23:00 -0700

    1 sub-directory [16 files]

---
+++ Music/1.1 Music (GitHub)/Part 2/20Essentials_GitHub/Unsorted/afrojack-main/config.js
@@ -0,0 +1,23 @@
+export const nameBand = 'AFROJACK';
+export const nameSongs = [
+	"DO OR DIE (REMIX)", /*1*/
+	"HEY", /*2*/
+	"TEN FEET TALL", /*3*/
+	"THE SPARK", /*4*/
+	"ANOTHER LIFE", /*5*/
+	"AS YOUR FRIEND", /*6*/
+	"SUMMERTHING", /*7*/
+	"USED TO HAVE IT ALL", /*8*/
+	"WAVE YOUR FLAG", /*9*/
+	"DOLLA $IGN", /*10*/
+	"HERO", /*11*/
+	"WE GOT THAT COOL", /*12*/
+	"DYNAMITE", /*13*/
+	"ANYWHERE WITH YOU", /*14*/
+	"ILLUMINATE", /*15*/
+	"NEW MEMORIES", /*16*/
+	"KEEP OUR LOVE ALIVE", /*17*/
+	"THE WAY WE SEE THE WORLD", /*18*/
+	"BOOM BOOM POW", /*19*/
+	"FREEDOM", /*20*/
+]
