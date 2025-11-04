7f61dbff92ab3be9fd04d8569cfd4e0e3c09d3bf
Sean P. Myrick V19.1.7.2 <seanwallawalla@protonmail.com>
2025-11-03 21:39:43 -0800

    1 sub-directory [16 files]

---
+++ Music/1.1 Music (GitHub)/Part 2/20Essentials_GitHub/Unsorted/jam-project-main/config.js
@@ -0,0 +1,23 @@
+export const nameBand = ' JAM PROJECT';
+export const nameSongs = [
+	"ONE PUNCH MAN - THE HERO!!", /*1*/
+	"BLOODLINES", /*2*/
+	"DREI KREUZ", /*3*/
+	"＜GARO＞", /*4*/
+	"SAVIOR IN THE DARK", /*5*/
+	"THE FINAL ROUND", /*6*/
+	"FIGHT BACK!", /*7*/
+	"GET OVER", /*8*/
+	"THE JUDGEMENT", /*9*/
+	"FINAL COUNTDOWN", /*10*/
+	"ONE CHANCE!", /*11*/
+	"SPIRITS OF BELLMARE", /*12*/
+	"OVER THE MAX", /*13*/
+	"SKILL-2015-", /*14*/
+	"静寂のアポストル", /*15*/
+	"THE BRAVE", /*16*/
+	"THE EXCEEDER ", /*17*/
+	"BREAK OUT", /*18*/
+	"BREAKTHROUGH", /*19*/
+	"THE AGE OF DRAGON KNIGHTS", /*20*/
+]
