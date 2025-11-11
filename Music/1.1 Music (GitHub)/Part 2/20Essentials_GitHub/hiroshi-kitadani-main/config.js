7440cb67b8703c5370e353884a1cb3aab314c466
Sean P. Myrick V19.1.7.2 <seanwallawalla@protonmail.com>
2025-11-10 20:55:39 -0800

    1 sub-directory [16 files]

---
+++ Music/1.1 Music (GitHub)/Part 2/20Essentials_GitHub/hiroshi-kitadani-main/config.js
@@ -0,0 +1,23 @@
+export const nameBand = 'HIROSHI KITADANI';
+export const nameSongs = [
+	"WE GO! LIVE", /*1*/
+	"WE ARE!", /*2*/
+	"あーーっす！", /*3*/
+	"ONE PIECE OPENING 15 WE GO", /*4*/
+	"BE THE ONE", /*5*/
+	"YOU BE ONE WINNER", /*6*/
+	"WAKE UP!", /*7*/
+	"THE HERO !!", /*8*/
+	"ヒカリヘ", /*9*/
+	"ココロのちず", /*10*/
+	"SAILING DAY", /*11*/
+	"BRAND NEW WORLD", /*12*/
+	"JUNGLE P", /*13*/
+	"DREAMIN' ON", /*14*/
+	"ONE DAY", /*15*/
+	"新時代", /*16*/
+	"MEMORIES", /*17*/
+	"DEEP RED", /*18*/
+	"聖なるけものたち", /*19*/
+	"IN FATE", /*20*/
+]
