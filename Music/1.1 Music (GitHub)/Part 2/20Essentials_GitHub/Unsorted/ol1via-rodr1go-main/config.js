0bb7a444a95dc804d7dc19a2c4f0f31149b7db71
Sean P. Myrick V19.1.7.2 <seanwallawalla@protonmail.com>
2025-11-24 19:32:31 -0800

    1 sub-directory [16 files] - 2025, November 24th

---
+++ Music/1.1 Music (GitHub)/Part 2/20Essentials_GitHub/Unsorted/ol1via-rodr1go-main/config.js
@@ -0,0 +1,23 @@
+export const nameBand = 'OLIVIA RODRIGO';
+export const nameSongs = [
+	"DRIVERS LICENSE", /*1*/
+	"DEJA VU", /*2*/
+	"GOOD 4 U", /*3*/
+	"HAPPIER", /*4*/
+	"TRAITOR", /*5*/
+	"VAMPIRE", /*6*/
+	"JEALOUSY, JEALOUSY", /*7*/
+	"FAVORITE CRIME", /*8*/
+	"BRUTAL", /*9*/
+	"GET HIM BACK!", /*10*/
+	"BAD IDEA RIGHT?", /*11*/
+	"CAN’T CATCH ME NOW", /*12*/
+	"ENOUGH FOR YOU", /*13*/
+	"1 STEP FORWARD, 3 STEPS BACK", /*14*/
+	"OBSESSED", /*15*/
+	"HOPE UR OK", /*16*/
+	"LACY", /*17*/
+	"THE GRUDGE", /*18*/
+	"SO AMERICAN", /*19*/
+	"BALLAD OF A HOMESCHOOLED GIRL", /*20*/
+]
