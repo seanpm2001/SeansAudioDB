ae3f555d1d6a9a057e0b7ff7a36a667795d3ae53
Sean P. Myrick V19.1.7.2 <seanwallawalla@protonmail.com>
2025-12-02 22:24:36 -0800

    1 sub-directory [16 files]

---
+++ Music/1.1 Music (GitHub)/Part 2/20Essentials_GitHub/Unsorted/prokyon-main/config.js
@@ -0,0 +1,23 @@
+export const nameBand = 'PROKYON';
+export const nameSongs = [
+	"DIE FOR YOU", /*1*/
+	"DIRT OFF YOUR SHOULDER", /*2*/
+	"HEAVY IS THE CROWN", /*3*/
+	"IT HAS BEGUN", /*4*/
+	"STARSET - DIE FOR YOU", /*5*/
+	"I WILL NOT BOW", /*6*/
+	"IT'S NOT OVER", /*7*/
+	"DYNAMICHOUR", /*8*/
+	"THE MYSTIC", /*9*/
+	"DIE FOR YOU [RISING WAVE COVER]", /*10*/
+	"THROUGH IT ALL", /*11*/
+	"THE PHOENIX", /*12*/
+	"WARRIOR INSIDE", /*13*/
+	"CROSSFIRE", /*14*/
+	"NO PURPOSE", /*15*/
+	"SURCONSOMMATION!", /*16*/
+	"WHY YOU CARE?", /*17*/
+	"MOSH PIT", /*18*/
+	"DISBELIEF", /*19*/
+	"PUSHING THE LIMITS", /*20*/
+]
