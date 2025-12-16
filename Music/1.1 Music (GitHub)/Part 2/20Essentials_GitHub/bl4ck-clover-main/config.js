defeabdcf34c1c78e8c9bfb077aba68b48a2994e
Sean P. Myrick V19.1.7.2 <seanwallawalla@protonmail.com>
2025-12-15 21:25:16 -0800

    1 sub-directory [16 files]

---
+++ Music/1.1 Music (GitHub)/Part 2/20Essentials_GitHub/bl4ck-clover-main/config.js
@@ -0,0 +1,23 @@
+export const nameBand = 'BLACK CLOVER';
+export const nameSongs = [
+	"V1 | BLACK CATCHER", /*1*/
+	"RIGHT NOW", /*2*/
+	"PAINT IT BLACK", /*3*/
+	"BLACK ROVER", /*4*/
+	"GUESS WHO IS BACK", /*5*/
+	"GAMUSHARA", /*6*/
+	"RAKUGAKI PAGE", /*7*/
+	"JUSTADICE", /*8*/
+	"HARUKA MIRAI", /*9*/
+	"V2 | BLACK CATCHER", /*10*/
+	"STORIES", /*11*/
+	"EVERLASTING SHINE", /*12*/
+	"GRANDEUR BY SNOW MAN", /*13*/
+	"POSSIBLE", /*14*/
+	"V3 | BLACK CATCHER", /*15*/
+	"AMAZING DREAMS", /*16*/
+	"BLACK TO THE DREAMLIGHT", /*17*/
+	"FOUR", /*18*/
+	"TENJOU TENGE", /*19*/
+	"MY SONG MY DAYS", /*20*/
+]
